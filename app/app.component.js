2e4fcba3f4c043e47d36a0a13c12cb17bab0134d
stefano kitano <S.Kouroupis@gmail.com>
2015-12-22 00:29:52 +0200

    lots of work done

--- app/app.component.js
+++ app/app.component.js
@@ -1,11 +1,16 @@
 /**
  * Created by tougo on 17/12/15.
  */
-angular.module('PropertyManager', ['md.data.table','ngMaterial'])
-.controller('HouseController', function($scope,$http, $q, $mdDialog, $mdMedia) {
-    $scope.title1 = 'TESTING';
+angular.module('PropertyManager', ['md.data.table', 'ngMaterial'])
+    .service('selectedProperties', function () {
+        var _dataObj = {};
+        this.dataObj = _dataObj;
+    })
+    .controller('HouseController', function ($scope, $http, $q, $mdDialog, $mdMedia, selectedProperties) {
+        $scope.title1 = 'TESTING';
         $scope.properties = [];
         $scope.selected = [];
+        selectedProperties.dataObj.properties = $scope.selected;
 
         var initProperties = [];
 
@@ -21,29 +26,22 @@ angular.module('PropertyManager', ['md.data.table','ngMaterial'])
         function readProperties() {
             var request = $http({
                 method: "get",
-                url: "/api/property/list",
-                params: {
-                    action: "add"
-                },
-                data: {
-                    name: name
-                }
+                url: "/api/property/list"
             });
-            return( request.then( handleSuccess, handleError ) );
+            return ( request.then(handleSuccess, handleError) );
         }
 
-        function handleSuccess( response ) {
-            return response.data ;
+        function handleSuccess(response) {
+            return response.data;
         }
 
-        function handleError( response ) {
+        function handleError(response) {
             if (
-                ! angular.isObject( response.data ) ||
-                ! response.data.message
+                !angular.isObject(response.data) || !response.data.message
             ) {
-                return( $q.reject( "An unknown error occurred." ) );
+                return ( $q.reject("An unknown error occurred.") );
             }
-            return( $q.reject( response.data.message ) );
+            return ( $q.reject(response.data.message) );
         }
 
         function loadProperties() {
@@ -55,10 +53,10 @@ angular.module('PropertyManager', ['md.data.table','ngMaterial'])
             );
         }
 
-        function applyProperties( newproperties ) {
+        function applyProperties(newproperties) {
             $scope.properties.total = newproperties.length;
             initProperties = newproperties;
-            $scope.properties = newproperties.slice(0,$scope.query.limit);
+            $scope.properties = newproperties.slice(0, $scope.query.limit);
             $scope.properties.total = initProperties.length;
         }
 
@@ -69,10 +67,10 @@ angular.module('PropertyManager', ['md.data.table','ngMaterial'])
 
         $scope.onOrderChange = function (order) {
             var asc = order.indexOf("-");
-            var field = order.split("-")[order.split("-").length-1];
+            var field = order.split("-")[order.split("-").length - 1];
 
             $scope.properties.sort(function (a, b) {
-                if(a[field]!== null && b[field]!== null) {
+                if (a[field] !== null && b[field] !== null) {
                     return a[field].toString().localeCompare(b[field].toString());
                 }
                 else {
@@ -87,31 +85,32 @@ angular.module('PropertyManager', ['md.data.table','ngMaterial'])
         };
 
         $scope.onPaginationChange = function (page, limit) {
-            $scope.properties = initProperties.slice((page-1)*limit,(page-1)*limit+limit);
+            $scope.properties = initProperties.slice((page - 1) * limit, (page - 1) * limit + limit);
             $scope.properties.total = initProperties.length;
         };
 
-        $scope.showProperty = function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.showProperty = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
             $mdDialog.show({
                 controller: DialogController,
                 templateUrl: 'app/forms/property.html',
                 parent: angular.element(document.body),
                 targetEvent: ev,
-                clickOutsideToClose:true,
+                clickOutsideToClose: true,
                 fullscreen: useFullScreen
             });
         };
     })
-    .config(function($mdThemingProvider) {
+    .config(function ($mdThemingProvider) {
         // Configure a dark theme with primary foreground yellow
         $mdThemingProvider.theme('docs-dark', 'default')
             .primaryPalette('yellow')
             .dark();
     })
-    .controller('TennantController', function($scope,$http, $q, $mdDialog, $mdMedia) {
+    .controller('TennantController', function ($scope, $http, $q, $mdDialog, $mdMedia, selectedProperties) {
         $scope.tenants = [];
         $scope.selected = [];
+        selectedProperties.dataObj.tenants = $scope.selected;
 
         var initTennants = [];
         $scope.query = {
@@ -124,29 +123,22 @@ angular.module('PropertyManager', ['md.data.table','ngMaterial'])
         function readProperties() {
             var request = $http({
                 method: "get",
-                url: "/api/tenant/list",
-                params: {
-                    action: "add"
-                },
-                data: {
-                    name: name
-                }
+                url: "/api/tenant/list"
             });
-            return( request.then( handleSuccess, handleError ) );
+            return ( request.then(handleSuccess, handleError) );
         }
 
-        function handleSuccess( response ) {
-            return response.data ;
+        function handleSuccess(response) {
+            return response.data;
         }
 
-        function handleError( response ) {
+        function handleError(response) {
             if (
-                ! angular.isObject( response.data ) ||
-                ! response.data.message
+                !angular.isObject(response.data) || !response.data.message
             ) {
-                return( $q.reject( "An unknown error occurred." ) );
+                return ( $q.reject("An unknown error occurred.") );
             }
-            return( $q.reject( response.data.message ) );
+            return ( $q.reject(response.data.message) );
         }
 
         function loadRemoteData() {
@@ -158,10 +150,10 @@ angular.module('PropertyManager', ['md.data.table','ngMaterial'])
             );
         }
 
-        function applyRemoteData( newproperties ) {
+        function applyRemoteData(newproperties) {
             $scope.tenants.total = newproperties.length;
             initTennants = newproperties;
-            $scope.tenants = newproperties.slice(0,$scope.query.limit);
+            $scope.tenants = newproperties.slice(0, $scope.query.limit);
             $scope.tenants.total = initTennants.length;
         }
 
@@ -172,10 +164,10 @@ angular.module('PropertyManager', ['md.data.table','ngMaterial'])
 
         $scope.onOrderChange = function (order) {
             var asc = order.indexOf("-");
-            var field = order.split("-")[order.split("-").length-1];
+            var field = order.split("-")[order.split("-").length - 1];
 
             $scope.tenants.sort(function (a, b) {
-                if(a[field]!== null && b[field]!== null) {
+                if (a[field] !== null && b[field] !== null) {
                     return a[field].toString().localeCompare(b[field].toString());
                 }
                 else {
@@ -190,51 +182,155 @@ angular.module('PropertyManager', ['md.data.table','ngMaterial'])
         };
 
         $scope.onPaginationChange = function (page, limit) {
-            $scope.tenants = initTennants.slice((page-1)*limit,(page-1)*limit+limit);
+            $scope.tenants = initTennants.slice((page - 1) * limit, (page - 1) * limit + limit);
             $scope.tenants.total = initTennants.length;
         };
 
-        $scope.showTenant= function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+        $scope.showTenant = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
             $mdDialog.show({
-                controller: DialogController,
+                controller: 'TennantController',
                 templateUrl: 'app/forms/tenant.html',
                 parent: angular.element(document.body),
                 targetEvent: ev,
-                clickOutsideToClose:true,
+                clickOutsideToClose: true,
                 fullscreen: useFullScreen
             });
         };
+
+        $scope.hide = function () {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        };
+        $scope.answer = function (answer) {
+            writeTenant()
+                .then(
+                function (tennant) {
+                });
+            $mdDialog.hide(answer);
+        };
+
+        function writeTenant() {
+            var request = $http({
+                method: "post",
+                url: "/api/tenant/add",
+                params: {
+                    first_name: $scope.tenant.first_name,
+                    last_name: $scope.tenant.last_name,
+                    afm: $scope.tenant.afm
+                }
+            });
+            return ( request.then(handleSuccess, handleError) );
+        }
+
     })
-    .controller('PaymentController', function($scope,$http, $q, $mdDialog, $mdMedia) {
+    .controller('PaymentController', function ($scope, $http, $q, $mdDialog, $mdMedia, selectedProperties) {
+
+        $scope.ConnectPropertyAndTenant = function (ev) {
+            var selectprop = 0;
+            var selecttent = 0;
+            if (selectedProperties.dataObj.properties) {
+                selectprop = selectedProperties.dataObj.properties.length;
+            }
+            if (selectedProperties.dataObj.tenants) {
+                selecttent = selectedProperties.dataObj.tenants.length;
+            }
+
+            if (selectprop !== 1 || selecttent !== 1) {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .parent(angular.element(document.querySelector('#popupContainer')))
+                        .clickOutsideToClose(true)
+                        .title('ΠΡΟΣΟΧΗ')
+                        .textContent('ΟΙ ΕΠΙΛΕΓΜΕΝΕΣ ΙΔΙΟΚΤΗΣΙΕΣ ΕΙΝΑΙ ' + selectprop
+                        + ' ΟI ΕΠΙΛΕΓΜΕΝΟI ΕΝΟΙΚΙΑΣΤEΣ ΕΙΝΑΙ ' + selecttent + ' ΚΑΜΙΑ ΕΝΕΡΓΕΙΑ ΔΕΝ ΕΙΝΑΙ ΔΥΝΑΤΗ')
+                        .ariaLabel('Alert Dialog Demo')
+                        .ok('ΟΚ')
+                        .targetEvent(ev)
+                );
+            }
+
+            if (selectedProperties.dataObj.properties && selectedProperties.dataObj.properties) {
+                if (selectedProperties.dataObj.properties.length === 1 && selectedProperties.dataObj.tenants.length === 1) {
+                    $mdDialog.show(
+                        $mdDialog.alert()
+                            .parent(angular.element(document.querySelector('#popupContainer')))
+                            .clickOutsideToClose(true)
+                            .title('ΠΡΟΣΟΧΗ')
+                            .textContent('ΕΙΣΤΑΙ ΣΙΓΟΥΡΟΙ ΟΤΙ Ο ' + selectedProperties.dataObj.tenants[0].last_name
+                            + ' ΕΝΟΙΚΙΑΣΕ ΤΟ ' + selectedProperties.dataObj.properties[0].streetname
+                            + ' ' + selectedProperties.dataObj.properties[0].streetnumber)
+                            .ariaLabel('Alert Dialog Demo')
+                            .ok('ΟΚ')
+                            .targetEvent(ev)
+                    );
+                }
+            }
+        }
+
+        $scope.showPayment = function (ev) {
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+            $mdDialog.show({
+                controller: 'PaymentController',
+                templateUrl: 'app/forms/payment.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                controllerAs: 'ctrl',
+                locals: {months: $scope.months},
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
+            });
+        };
+        $scope.hide = function () {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function () {
+            $mdDialog.cancel();
+        };
+        $scope.answer = function (answer, type) {
+            writePayment()
+                .then(
+                function (payment) {
+                });
+            $mdDialog.hide(answer);
+        };
+
+        function writePayment() {
+            var request = $http({
+                method: "post",
+                url: "/api/payment/add",
+                params: {
+                    amount: $scope.payment.first_name,
+                    paydate: $scope.payment.paydate,
+                    month: $scope.payment.month
+                }
+            });
+            return ( request.then(handleSuccess, handleError) );
+        }
+
         $scope.months = [];
         loadMonths();
         function readMonths() {
             var request = $http({
                 method: "get",
-                url: "/api/months/list",
-                params: {
-                    action: "add"
-                },
-                data: {
-                    name: name
-                }
+                url: "/api/months/list"
             });
-            return( request.then( handleSuccess, handleError ) );
+            return ( request.then(handleSuccess, handleError) );
         }
 
-        function handleSuccess( response ) {
-            return response.data ;
+        function handleSuccess(response) {
+            return response.data;
         }
 
-        function handleError( response ) {
+        function handleError(response) {
             if (
-                ! angular.isObject( response.data ) ||
-                ! response.data.message
+                !angular.isObject(response.data) || !response.data.message
             ) {
-                return( $q.reject( "An unknown error occurred." ) );
+                return ( $q.reject("An unknown error occurred.") );
             }
-            return( $q.reject( response.data.message ) );
+            return ( $q.reject(response.data.message) );
         }
 
         function loadMonths() {
@@ -246,34 +342,76 @@ angular.module('PropertyManager', ['md.data.table','ngMaterial'])
             );
         }
 
-        function applyMonths(newproperties ) {
+        function applyMonths(newproperties) {
             $scope.months = newproperties;
         }
-
-        $scope.showPayment= function(ev) {
-            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
-            $mdDialog.show({
-                controller: DialogController,
-                templateUrl: 'app/forms/payment.html',
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                controller: 'PaymentController',
-                controllerAs: 'ctrl',
-                locals: { months: $scope.months },
-                clickOutsideToClose:true,
-                fullscreen: useFullScreen
-            });
-        };
     });
 
-function DialogController($scope, $mdDialog) {
-    $scope.hide = function() {
+
+function DialogController($scope, $mdDialog, $http, $q) {
+    $scope.hide = function () {
         $mdDialog.hide();
     };
-    $scope.cancel = function() {
+    $scope.cancel = function () {
         $mdDialog.cancel();
     };
-    $scope.answer = function(answer) {
+    $scope.answer = function (answer, type) {
+        if (type === 'TENANT') {
+            writeTenant()
+                .then(
+                function (tennant) {
+                    applyMonths(tennant);
+                });
+        }
         $mdDialog.hide(answer);
     };
+
+    function writeTenant() {
+        var request = $http({
+            method: "post",
+            url: "/api/tenant/add",
+            params: {
+                first_name: $scope.tenant.first_name,
+                last_name: $scope.tenant.last_name,
+                afm: $scope.tenant.afm
+            }
+        });
+        return ( request.then(handleSuccess, handleError) );
+    }
+
+    $scope.months = [];
+    loadMonths();
+    function readMonths() {
+        var request = $http({
+            method: "get",
+            url: "/api/months/list"
+        });
+        return ( request.then(handleSuccess, handleError) );
+    }
+
+    function handleSuccess(response) {
+        return response.data;
+    }
+
+    function handleError(response) {
+        if (
+            !angular.isObject(response.data) || !response.data.message
+        ) {
+            return ( $q.reject("An unknown error occurred.") );
+        }
+        return ( $q.reject(response.data.message) );
+    }
+
+    function loadMonths() {
+        readMonths()
+            .then(
+            function (months) {
+                applyMonths(months);
+            }
+        );
+    }
+
+    function applyMonths(newproperties) {
+        $scope.months = newproperties;
+    }
 }

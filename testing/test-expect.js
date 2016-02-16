8459008c60001d923901802b4f91ed41de795b4a
stefano kitano <S.Kouroupis@gmail.com>
2016-02-16 22:51:02 +0200

    mocha testing

---
+++ testing/test-expect.js
@@ -0,0 +1,39 @@
+/**
+ * Created by tougo on 16/2/16.
+ */
+var supertest = require("supertest");
+var server = supertest.agent("http://localhost:3000");
+
+describe('property list', function() {
+    it('should respond to property list', function () {
+        server
+            .get("/api/property/list")
+            .expect("Content-type",/json/)
+            .expect(200) // THis is HTTP response
+            .end(function(err,res){
+                // HTTP status should be 200
+                res.status.should.equal(200);
+                // Error key should be false.
+                res.body.error.should.equal(false);
+                done();
+            });
+    });
+
+});
+
+describe('tenant list', function() {
+    it('should respond to tenant list', function () {
+        server
+            .get("/api/tenant/list")
+            .expect("Content-type",/json/)
+            .expect(200) // THis is HTTP response
+            .end(function(err,res){
+                // HTTP status should be 200
+                res.status.should.equal(200);
+                // Error key should be false.
+                res.body.error.should.equal(false);
+                done();
+            });
+    });
+
+});
